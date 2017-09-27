80c18bcf450a13c715f498c6ac6f09540880c738
jhayarevalo <jhayarevalo@gmail.com>
2017-09-27 02:10:10 -0400

    Initial Commit

---
+++ skeleton.cpp
@@ -0,0 +1,151 @@
+#include "skeleton.h"
+#include "splitstring.h"
+#include <cmath>
+
+/*
+ * Load skeleton file
+ */
+void Skeleton::loadSkeleton(std::string skelFileName)
+{
+    std::string strBone;
+    std::ifstream skelFile(skelFileName.c_str());
+    if (skelFile.is_open())
+    {
+        while ( std::getline(skelFile, strBone)) { //Read a line to build a bone
+            std::vector<std::string> boneParams;
+            splitstring splitStr(strBone);
+            boneParams = splitStr.split(' ');
+            Joint temp;
+            temp.position.x = std::atof(boneParams[1].c_str());
+            temp.position.y = std::atof(boneParams[2].c_str());
+			temp.position.z = std::atof(boneParams[3].c_str());
+			temp.indexParent = std::atoi(boneParams[4].c_str());
+
+
+            if (std::atoi(boneParams[0].c_str()) != joints.size())
+            {
+                std::cout<<"[Warning!!!] Bone index not match\n";
+            }
+            joints.push_back(temp);
+        }
+    }
+}
+
+/*
+ * Load Animation
+ */
+void Skeleton::loadAnimation(std::string skelFileName)
+{
+}
+
+
+/*
+ * Draw skeleton with OpenGL
+ */
+void Skeleton::glDrawSkeleton()
+{
+    //Rigging skeleton
+    glDisable(GL_DEPTH_TEST);
+    
+    glPushMatrix();
+    glTranslatef(-0.9,-0.9,-0.9);
+	glScalef(1.8,1.8,1.8);
+	glPointSize(6);
+	glColor3f(1,0,0);
+    updateScreenCoord();
+    
+    for (unsigned i=0; i<joints.size(); i++)
+    {
+        if (joints[i].isPicked)
+            glColor3f(1.0, 0.0, 0.0);
+        else if (joints[i].isHovered)
+            glColor3f(0.7, 0.7, 0.7);
+        else
+            glColor3f(0.3, 0.3, 0.3);
+
+        glTranslated(joints[i].position.x, joints[i].position.y, joints[i].position.z);
+        glutSolidSphere(0.01, 15, 15);
+        glTranslated(-joints[i].position.x, -joints[i].position.y, -joints[i].position.z);
+
+    }
+	glBegin(GL_LINES);
+	for (int i = 0; i < joints.size(); i++) {
+		if (joints[i].indexParent > -1) {
+			glVertex3f(joints[i].position.x, joints[i].position.y, joints[i].position.z);
+			glVertex3f(joints[joints[i].indexParent].position.x, joints[joints[i].indexParent].position.y, joints[joints[i].indexParent].position.z);
+		}
+	}
+	glEnd();
+
+    glPopMatrix();
+    
+    glEnable(GL_DEPTH_TEST);
+}
+
+void Skeleton::updateScreenCoord()
+{
+    GLint viewport[4];
+    GLdouble modelview[16];
+    GLdouble projection[16];
+    GLdouble winX, winY, winZ;
+
+    glGetDoublev( GL_MODELVIEW_MATRIX, modelview );
+    glGetDoublev( GL_PROJECTION_MATRIX, projection );
+    glGetIntegerv( GL_VIEWPORT, viewport );
+    for (unsigned i=0; i<joints.size(); i++)
+    {
+        gluProject((GLdouble)joints[i].position.x, (GLdouble)joints[i].position.y, (GLdouble)joints[i].position.z,
+                modelview, projection, viewport,
+                &winX, &winY, &winZ );
+        joints[i].screenCoord.x = winX;
+        joints[i].screenCoord.y = (double)glutGet(GLUT_WINDOW_HEIGHT)-winY;
+    }
+}
+void Skeleton::checkHoveringStatus(int x, int y)
+{
+    double distance = 0.0f;
+    double minDistance = 1000.0f;
+    int hoveredJoint = -1;
+    for(unsigned i=0; i < joints.size(); i++)
+    {
+        joints[i].isHovered = false;
+        distance = sqrt((x - joints[i].screenCoord.x)*(x - joints[i].screenCoord.x) 
+                + (y - joints[i].screenCoord.y)*(y - joints[i].screenCoord.y));
+        if (distance > 50) continue;
+        if (distance < minDistance)
+        {
+            hoveredJoint = i;
+            minDistance = distance;
+        }
+    }
+    if (hoveredJoint != -1) joints[hoveredJoint].isHovered = true;
+}
+
+void Skeleton::release()
+{
+    hasJointSelected = false;
+    for (unsigned i=0; i<joints.size(); i++)
+    {
+        joints[i].isPicked = false;
+    }
+}
+
+void Skeleton::selectOrReleaseJoint()
+{
+    bool hasHovered=false;
+    for (unsigned i=0; i<joints.size(); i++)
+    {
+        joints[i].isPicked = false;
+        if (joints[i].isHovered)
+        {
+            hasHovered = true;
+            joints[i].isPicked = true;
+            hasJointSelected = true;
+        }
+    }
+    if (!hasHovered)    //Release joint
+        hasJointSelected = false;
+}
+
+
+
---
+++ skeleton.h
@@ -0,0 +1,125 @@
+#ifndef SKELETON_H
+#define SKELETON_H
+#include <vector>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+#include <iostream>
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#ifdef _WIN32
+#include "GL/glut.h"
+#else
+#include <GL/freeglut.h>
+#endif
+#endif
+
+#include "simpleMath.h"
+#include <Eigen/Eigen>
+#include <Eigen/StdVector>
+
+using namespace std;
+
+struct Joint
+{
+	Eigen::Matrix4f localTransformation = Eigen::Matrix4f::Identity();
+	Eigen::Matrix4f globalTransformation = Eigen::Matrix4f::Identity();
+
+	int indexParent;
+    Vec3 position;
+    Vec2 screenCoord;
+    bool isHovered;
+    bool isPicked;
+    
+    Joint()
+    {
+        isHovered = false;
+        isPicked = false;
+    }
+
+	void addTransformation(Eigen::Vector3f& axis, double angle) {
+		Eigen::Matrix4f rotationMatrix;
+		Eigen::Vector4f positionMatrix = Eigen::Vector4f(position.x, position.y, position.z, 0.0f);
+
+		rotationMatrix(0,0) = pow(axis.x(), 2) + cos(angle) * (1 - pow(axis.x(), 2));
+		rotationMatrix(0, 1) = (axis.x())*(axis.y())*(1 - cos(angle)) - (axis.z())*sin(angle);
+		rotationMatrix(0, 2) = (axis.x())*(axis.z())*(1 - cos(angle)) + (axis.y())*sin(angle);
+
+		rotationMatrix(1, 0) = (axis.x())*(axis.y())*(1 - cos(angle)) + (axis.z())*sin(angle);
+		rotationMatrix(1, 1) = pow(axis.y(), 2) + cos(angle) * (1 - pow(axis.y(), 2));
+		rotationMatrix(1, 2) = (axis.y())*(axis.z())*(1 - cos(angle)) - (axis.x())*sin(angle);
+
+		rotationMatrix(2, 0) = (axis.x())*(axis.z())*(1 - cos(angle)) - (axis.y())*sin(angle);
+		rotationMatrix(2, 1) = (axis.y())*(axis.z())*(1 - cos(angle)) - (axis.x())*sin(angle);
+		rotationMatrix(2, 2) = pow(axis.z(), 2) + cos(angle) * (1 - pow(axis.z(), 2));
+
+		rotationMatrix(0, 3) = rotationMatrix(1, 3) = rotationMatrix(2, 3) = 0;
+		rotationMatrix(3, 0) = rotationMatrix(3, 1) = rotationMatrix(3, 2) = 0;
+
+		rotationMatrix(3, 3) = 1;
+
+		positionMatrix = rotationMatrix * positionMatrix;
+
+		position.x = positionMatrix.x();
+		position.y = positionMatrix.y();
+		position.z = positionMatrix.z();
+
+		cout << "x: " << position.x << endl;
+		cout << "y: " << position.y << endl;
+		cout << "z: " << position.z << endl << endl;
+	}
+
+	void computeGlobalTransformation() {
+		globalTransformation = globalTransformation * localTransformation;
+	}
+
+};
+
+class Skeleton
+{
+public:
+    std::vector<Joint, Eigen::aligned_allocator<Joint>> joints;
+    /*Update screen coordinates of joints*/
+    void updateScreenCoord();
+    
+public:
+    /*True if the skeleton has a joint selected*/
+    bool hasJointSelected;   
+    Skeleton(){hasJointSelected = false;};
+    /*
+     * Load Skeleton file
+     */
+    void loadSkeleton(std::string skelFileName);
+
+    /*
+     * Load animation file
+     */
+    void loadAnimation(std::string skelFileName);
+
+    /*
+     * Draw skeleton with OpenGL
+     */
+    void glDrawSkeleton();
+
+    /*
+     * Check if any joint is hovered by given mouse coordinate
+     */
+    void checkHoveringStatus(int x, int y);
+
+    void release();
+    
+    void selectOrReleaseJoint();
+
+	std::vector<Joint, Eigen::aligned_allocator<Joint>> getJoints() {
+		return joints;
+	}
+
+	void setJointPosition(int index) {
+
+	}
+
+};
+
+#endif
